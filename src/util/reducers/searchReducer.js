176d51dc04283d09513594140ff2faae8beb05bb
foreverChoi <foreverchoi0706@gmail.com>
2021-01-19 02:03:32 +0900

    refactoring

---
+++ src/util/reducers/searchReducer.js
@@ -0,0 +1,68 @@
+import { put, delay, call, takeLatest, takeEvery } from "redux-saga/effects";
+import lolAPI from "../api/lolAPI";
+
+const SEARCH = "SEARCH";
+
+const SEARCH_DONE = "SEARCH_DONE";
+
+const MATCH_LIST = "MATCH_LIST";
+
+const MATCH_LIST_SUCCESS = "MATCH_LIST_SUCCESS";
+
+export const search = (keyword) => ({ type: SEARCH, keyword });
+
+const searchDone = (data) => ({ type: SEARCH_DONE, data });
+
+function* handleSearch(action) {
+  yield delay(1000);
+  const data = yield call(lolAPI.searchSummoner, action.keyword);
+  yield put(searchDone(data));
+}
+
+export const matchList = (accountId) => ({ type: MATCH_LIST, accountId });
+
+const matchListSuccess = (data) => ({ type: MATCH_LIST_SUCCESS, data });
+
+function* handleMatchList(action) {
+  const data = yield call(lolAPI.matchList, action.accountId);
+  yield put(matchListSuccess(data));
+}
+
+export function* searchSaga() {
+  yield takeLatest(SEARCH, handleSearch);
+  yield takeEvery(MATCH_LIST, handleMatchList);
+}
+
+const initialState = {
+  result: {
+    isSearched: false,
+    data: null,
+  },
+  matchList: {
+    isLoaded: false,
+    data: null,
+  },
+};
+
+export default function searchReducer(state = initialState, action) {
+  switch (action.type) {
+    case SEARCH:
+      return {
+        result: {
+          isSearched: false,
+          data: null,
+        },
+      };
+    case SEARCH_DONE:
+      return {
+        result: {
+          isSearched: true,
+          data: action.data,
+        },
+      };
+    case MATCH_LIST_SUCCESS:
+      return state;
+    default:
+      return state;
+  }
+}
