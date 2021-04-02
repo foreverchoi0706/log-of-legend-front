0620255d2c1c03bc980fc165d2bd297fe2af5704
foreverChoi <foreverchoi0706@gmail.com>
2021-04-03 02:20:37 +0900

    logger delete

--- log-of-legend-front/src/index.js
+++ log-of-legend-front/src/index.js
@@ -3,14 +3,13 @@ import reactDom from "react-dom";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
 import createSagaMiddleware from "redux-saga";
 
 import rootReducer, { rootSaga } from "./util/reducers/rootReducer";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware,logger));
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(rootSaga);
 
